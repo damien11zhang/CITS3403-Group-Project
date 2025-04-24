4782afec522866e0da8568ad5a5c83432c0479e3
Damien Danming Zhang <damien11zhang@gmail.com>
2025-04-24 16:44:35 +0800

    Updated app.py

--- frontend/app.py
+++ frontend/app.py
@@ -14,5 +14,29 @@ def base():
 def test():
     return render_template("test.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/support')
+def support():
+    return render_template("support.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+@app.route('/quiz')
+def quiz():
+    return render_template("quiz.html")
+
+@app.route('/results')
+def results():
+    return render_template("results.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
