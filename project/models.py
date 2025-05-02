7697e74592281b06abf4fa5844646bc4104e5c36
dame <damien11zhang@gmail.com>
2025-05-02 10:18:16 +0800

    Added Question class into models.py

--- project/models.py
+++ project/models.py
@@ -1,6 +1,3 @@
-from flask_sqlalchemy import SQLAlchemy
-
-# Create an instance of SQLAlchemy
 from app import db
 
 class JobCluster(db.Model):
@@ -13,23 +10,30 @@ class Subgroup(db.Model):
     __tablename__ = 'subgroups'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    question = db.Column(db.String(300), nullable=False)
     job_cluster_id = db.Column(db.Integer, db.ForeignKey('job_clusters.id'), nullable=False)
     jobs = db.relationship('Job', backref='subgroup', lazy=True)
+    questions = db.relationship('Question', backref='subgroup', lazy=True)
 
 class Job(db.Model):
     __tablename__ = 'jobs'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    question_1 = db.Column(db.String(300), nullable=False)
-    question_2 = db.Column(db.String(300), nullable=False)
     subgroup_id = db.Column(db.Integer, db.ForeignKey('subgroups.id'), nullable=False)
+    questions = db.relationship('Question', backref='job', lazy=True)
+
+class Question(db.Model):
+    __tablename__ = 'questions'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(300), nullable=False)
+    question_type = db.Column(db.String(20), nullable=False)  # 'first', 'second', or 'subgroup'
+    job_id = db.Column(db.Integer, db.ForeignKey('jobs.id'), nullable=True)
+    subgroup_id = db.Column(db.Integer, db.ForeignKey('subgroups.id'), nullable=True)
 
 class UserResponse(db.Model):
     __tablename__ = 'user_responses'
     id = db.Column(db.Integer, primary_key=True)
-    session_id = db.Column(db.String(100), nullable=False)  # To track users anonymously
-    question_type = db.Column(db.String(50), nullable=False)  # e.g., 'subgroup', 'job_q1', 'job_q2'
-    target_id = db.Column(db.Integer, nullable=False)  # ID of the subgroup/job
-    score = db.Column(db.Integer, nullable=False)  # Score out of 10
+    session_id = db.Column(db.String(100), nullable=False)
+    question_type = db.Column(db.String(50), nullable=False)
+    target_id = db.Column(db.Integer, nullable=False)  # ID of job or subgroup
+    score = db.Column(db.Integer, nullable=False)
     timestamp = db.Column(db.DateTime, server_default=db.func.now())
