0c8da02c76b7f23d7f71bf828f28615471c55e50
durianeater <swootinator@gmail.com>
2025-05-14 21:08:08 +0800

    fixed signupform in forms.py

--- project/forms.py
+++ project/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
