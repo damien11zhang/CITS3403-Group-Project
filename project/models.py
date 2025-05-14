9d0a4b59ebea6fa51eee6b15b14c60db4677c970
dame <damien11zhang@gmail.com>
2025-05-14 17:40:23 +0800

    testing

--- project/models.py
+++ project/models.py
@@ -12,11 +12,11 @@ print("Defining friendships table")
 
 class User(UserMixin, db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False, unique=True)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
-    bio = db.Column(db.Text, nullable=True)
+    id = db.Column(db.Integer, primary_key=True, extend_existing=True)
+    username = db.Column(db.String(100), nullable=False, unique=True, extend_existing=True)
+    email = db.Column(db.String(100), nullable=False, unique=True, extend_existing=True)
+    password = db.Column(db.String(100), nullable=False, extend_existing=True)
+    bio = db.Column(db.Text, nullable=True, extend_existing=True)
 
     friends = db.relationship(
         'User',
