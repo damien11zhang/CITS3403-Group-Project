d8b4d9466f0e1d8a659ee299395e394f5b50c26d
dame <damien11zhang@gmail.com>
2025-05-14 17:38:26 +0800

    changed test file

--- project/tests/test_friends.py
+++ project/tests/test_friends.py
@@ -2,7 +2,7 @@
 import pytest
 from project.app import app as flask_app
 from project.extensions import db as _db
-from project.models import friendships, User, FriendRequest
+from project.models import User, FriendRequest
 
 @pytest.fixture
 def app():
